079deaef8d085c784ed1b9af5d662bba7c355232
Kevin Lee <blackseeds10@hotmail.com>
2022-03-09 21:11:17 +0000

    Grabs the middle of  the aray be it 1 or 2 elements depending if even or odd number of elements

---
+++ middle.js
@@ -0,0 +1,39 @@
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const middle = function(num) {
+  let middleArray = [];
+  if (num.length <= 2) {
+    return middleArray;
+  }
+  if (num.length % 2 === 0) {
+    let middleTwo = num.length / 2;
+    middleArray.push(num[middleTwo] - 1);
+    middleArray.push(num[middleTwo])
+  } else {
+    let middle = ((num.length + 1) / 2) - 1;
+    middleArray.push(num[middle]);
+  }
+  return middleArray;
+};
+
+assertArraysEqual(middle([1]), []); // => []
+assertArraysEqual(middle([1, 2]), []); // => []
+assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
