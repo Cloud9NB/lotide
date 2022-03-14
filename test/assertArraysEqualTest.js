d53f5b45f8a4fe603709fe1fa564fd649a1da955
Kevin Lee <blackseeds10@hotmail.com>
2022-03-14 23:17:49 +0000

    refactored

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -3,6 +3,6 @@ const eqArrays = require('../eqArrays.js');
 
 
 assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
+assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertArraysEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
 assertArraysEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
