6a757666b39818db3f245980d1366e5b02cafa22
Kevin Lee <blackseeds10@hotmail.com>
2022-03-14 21:11:26 +0000

    refactored

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
 const eqArrays = function(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
@@ -18,14 +6,5 @@ const eqArrays = function(arr1, arr2) {
   }
   return true;
 };
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)
+module.exports = eqArrays;
