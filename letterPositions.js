4131123a55d4e1700ff27223151138043a58cdb8
Kevin Lee <blackseeds10@hotmail.com>
2022-03-15 04:24:04 +0000

    a

--- letterPositions.js
+++ letterPositions.js
@@ -1,10 +1,10 @@
-// if i pass the word hello, what is the final answer gonna look like
-//{ h: [0],
-//  e: [1],
-//  l: [2, 3]
-//  o: [4] }
-
-// dot notation vs bracket notation in javascript Objects GOOGLE!!
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
 
 const letterPositions = function(sentence) {
   const results = {};
@@ -20,20 +20,5 @@ const letterPositions = function(sentence) {
   console.log('result: ', results);
   return results;
 };
-letterPositions("lighthouse in the house")
-// const assertArraysEqual = function(arr1, arr2) {
-//   if (eqArrays(arr1, arr2)) {
-//     console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-//   } else {
-//     console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-//   }
-// };
 
-// const eqArrays = function(arr1, arr2) {
-//   for (let i = 0; i < arr1.length; i++) {
-//     if (arr1[i] !== arr2[i]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
+module.exports = letterPositions;
