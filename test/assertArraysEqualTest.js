b94ab087b6d6312c47faf251c8ae673e1f5f4c81
Kevin Lee <blackseeds10@hotmail.com>
2022-03-15 01:06:36 +0000

    refactored

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,17 +1,8 @@
 const assertArraysEqual = require('../assertArraysEqual');
-const assert = require('chai').assert;
+const eqArrays = require('../eqArrays.js');
 
-describe('#assertArraysEqual function', () => {
-  it('returns 1, 2, 3 for [1, 2, 3]', () => {
-    assert.strictEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]));
-  });
 
-  it ('returns 1, 2, 3 for [1, 2, 3]', () => {
-    assert.strictEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]));
-  });
-
-  it ('returns 1, 2, 3 for [\'1\', \'2\', \'3\']', () => {
-    assert.strictEqual(assertArraysEqual(['1', '2', '3'], ['1', '2', '3']));
-  });
-
-});
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(['1', '2', '3'], ['1', '2', '3']);
+assertArraysEqual(['1', '2', '3'], ['1', '2', 3]);
