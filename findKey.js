3ecbbb4f1f9786e62713f50c869d9ba5746adac7
Kevin Lee <blackseeds10@hotmail.com>
2022-03-15 04:18:46 +0000

    a

--- findKey.js
+++ findKey.js
@@ -8,25 +8,12 @@ const assertEqual = function(actual, expected) {
 
 const findKey = function(objects, callback) {
   for (const keys in objects) {
-    // console.log('keys: ',keys)
-    // console.log('object[keys]: ', objects[keys])
+    console.log('keys: ',keys);
+    console.log('object[keys]: ', objects[keys]);
     if (callback(objects[keys])) {
       return keys;
     }
   }
 };
 
-const result = findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma"
-
-assertEqual(result, 'noma');
-assertEqual(result, ['w', 't', '4', 'a', 'd']);
-
-// It should scan the object and return the first key for which the callback returns a truthy value.
-// If no key is found, then it should return undefined.
+module.exports = findKey;
