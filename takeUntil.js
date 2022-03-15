62602a20f355ef511905ff80c655187122c4b537
Kevin Lee <blackseeds10@hotmail.com>
2022-03-15 04:26:58 +0000

    a

--- takeUntil.js
+++ takeUntil.js
@@ -26,22 +26,7 @@ const takeUntil = function(array, callback) {
     }
     result.push(items)
   }
+  return result;
 };
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
-assertArraysEqual(results1, ['w', 't', '4', 'a', 'd']);
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
-assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
-assertArraysEqual(results2, ['w', 't', '4', 'a', 'd']);
-// Expected Output
-// [ 1, 2, 5, 7, 2 ]
-// --
-// [ 'I\'ve', 'been', 'to', 'Hollywood' ]
+module.exports = takeUntil;
